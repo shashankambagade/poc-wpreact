ebf06c91a4d5f2114a7d1007f5b0e2c6854b818d
Veeraj Jadhav <veerajjadhav1991@gmail.com>
2025-07-09 13:08:33 +0530

    Initial commit for headless CMS React setup

--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -1,16 +1,25 @@
-// /src/pages/Home.jsx
+// /src/pages/Page.jsx
 import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { getPageWithACF } from '../api/wp';
 import PageBuilder from '../components/PageBuilder';
 import Seo from '../components/Seo';
 
-export default function Home() {
+export default function Page() {
+  const { slug } = useParams();
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    getPageWithACF('home').then(setData);
-  }, []);
+    getPageWithACF(slug).then(res => {
+      if (res && res.length > 0) {
+        setData(res[0]);
+      } else {
+        setData(false); // not found
+      }
+    });
+  }, [slug]);
 
+  if (data === false) return <div>Page not found</div>;
   if (!data) return <div>Loading...</div>;
 
   return (
